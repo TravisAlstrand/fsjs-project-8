014b8a96051fe6e7949c94f7b5cc5ade8a766024
TravisAlstrand <travisalstrand@gmail.com>
2022-06-03 09:46:30 -0700

    Com.4
    Attempts started to render the index pug view file, and print db to page... db is still empty.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const allBooks = await book.findAll();
+  res.render('index', {books: allBooks, title: "Library Assistant"});
+}));
 
 module.exports = router;
